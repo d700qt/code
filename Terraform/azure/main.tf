5dda6b746e95d4ffdd97fed613f3e381ca1340f3
Ade Weetman <ade.weetman+it@gmail.com>
2018-05-04 20:43:55 +0100

    started main.tf for azure

--- Terraform/azure/main.tf
+++ Terraform/azure/main.tf
@@ -6,6 +6,8 @@ terraform {
 }
 */
 
+# based on https://docs.microsoft.com/en-us/azure/virtual-machines/linux/terraform-install-configure
+
 resource "azurerm_resource_group" "test" {
   name     = "acctestrg"
   location = "West US 2"
