0300b0210c89ab86343a4e174e5f32b7bafb0071
Adrian Weetman <adrianweetman@paypoint.com>
2018-05-09 20:45:24 +0100

    Attempt to copy files to server with old version of PS

--- Terraform/azure/setup/configure-vm.ps1
+++ Terraform/azure/setup/configure-vm.ps1
@@ -1,17 +1,11 @@
 $creds = get-credential
 $vm = read-host -Prompt "Please enter IP address"
 
-#set-item wsman:\localhost\Client\TrustedHosts -value $vm
+set-item wsman:\localhost\Client\TrustedHosts -value 1.2.3.4
+get-item wsman:\localhost\Client\TrustedHosts
+$pssession = New-PSSession -ComputerName $vm -Credential $creds
 
-$psSession = New-PSSession -ComputerName $vm -Credential $creds
-Invoke-Command -Session $psSession -ScriptBlock {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-Invoke-Command -Session $psSession -ScriptBlock {
-    choco feature enable -n allowGlobalConfirmation
-    choco install git
-    choco install visualstudiocode
-    choco install 7zip
-    choco install jdk8
-    #choco install azure-cli
-}
+new-item -path "c:\bootstrap" -type Directory
+Copy-Item "C:\code\Bamboo\atlassian-bamboo-6.5.0.zip" -Destination "C:\bootstrap" -ToSession $pssession
+
+new-psdrive -Name "H" -PSProvider "FileSystem" -Root "\\$vm\c$" -Credential $creds
