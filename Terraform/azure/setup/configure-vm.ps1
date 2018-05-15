40442567470fb96e4a1ee7c88d989a5cceb16eff
Adrian Weetman <adrianweetman@paypoint.com>
2018-05-15 18:44:18 +0100

    update to manual steps

--- Terraform/azure/setup/configure-vm.ps1
+++ Terraform/azure/setup/configure-vm.ps1
@@ -6,9 +6,9 @@ Bamboo config
 Installed VS 2017 Community edition, specifically:
 - Testing tools core features
 - MS build (+ some automatically selected dependencies
-- PLus the core VS IDE editor
+- Plus the core VS IDE editor
+- .NET 4.6.1 Targeting pack (to get the HelloWorld VS project working - the VS project targets 4.6.1)
 #>
-
 
 $creds = get-credential
 $vm = read-host -Prompt "Please enter hostname or IP address"
